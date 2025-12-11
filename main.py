c5d37b44b4371f42d9888d81b9188dc815feff06
hoppspa-spec <hoppspa@gmail.com>
2025-12-10 21:46:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "change-me-please")
 ALGORITHM  = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 class User(Base):
