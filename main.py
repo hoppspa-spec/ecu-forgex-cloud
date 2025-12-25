7b64df9b56270eed17c5efa53b9d0aa70c563b2a
hoppspa-spec <hoppspa@gmail.com>
2025-12-25 00:08:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import zipfile
+import requests
+from fastapi import UploadFile, File
+from fastapi.responses import HTMLResponse
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
